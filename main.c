1a78ef1d00d8a360dbd3c8ce6eb830149535863b
Purinpongpanich <123047827+Purinpongpanich@users.noreply.github.com>
2023-02-08 10:26:05 +0000

    last

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ int main(int argc, const char * argv[]) {
     p = head;
     while(p != NULL){
       tmp = p;
-      printf("\ndeclaring %p",tmp);
+      printf("\ndeclaring %d",tmp->value);
       p = p->next;
       free(tmp);
       }
